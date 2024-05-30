df18b1c473d2403166bba48b5d9e6605b0c15e0d
sudhakar961612 <s3xhdhh@gmail.com>
2024-05-30 17:06:31 +0530

    done

--- src/components/MovieListHeader.jsx
+++
@@ -1,9 +0,0 @@
-const MovieListHeader = ({ heading }) => {
-  return (
-    <div className='col'>
-      <h1>{heading}</h1>
-    </div>
-  );
-};
-
-export default MovieListHeader;
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,7 +1,9 @@
-const SearchBox = ({ searchValue, setSearchValue }) => {
+import React from "react";
+
+const SearchBox = (props) => {
   return (
     <div className='col col-sm-4'>
-      <input className='form-control' value={searchValue} onChange={(event) => setSearchValue(event.target.value)} placeholder='Type to search...' />
+      <input className='form-control' value={props.value} onChange={(event) => props.setSearchValue(event.target.value)} placeholder='Type to search...'></input>
     </div>
   );
 };
